b473ac01e5be640e2648d879b67dcf608f656321
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-09-19 18:39:01 +0300

    Setup controller templates and add to routs in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FamilieController;
+use App\Http\Controllers\FamilielidController;
+use App\Http\Controllers\SoortLidController;
+use App\Http\Controllers\ContributieController;
+use App\Http\Controllers\BoekjaarController;
 
+// Homepagina (optioneel)
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/families');
 });
+
+// CRUD routes (resource = alle standaard CRUD operaties)
+Route::resource('families', FamilieController::class);
+Route::resource('familieleden', FamilielidController::class);
+Route::resource('soortenleden', SoortLidController::class);
+Route::resource('contributies', ContributieController::class);
+Route::resource('boekjaren', BoekjaarController::class);
